bbf345defa8fa61567bbdc3b2369a51b483f5798
vietbui1502 <buivanviet93@gmail.com>
2024-07-02 22:55:05 +0700

    Basic project structure

---
+++ app/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "my_lib.h"
+#include "config.hpp"
+
+int main() {
+    std::cout << project_name << '\n';
+    print_blabla();
+    return 0;
+}
---
+++ src/my_lib/my_lib.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "my_lib.h"
+
+void print_blabla() {
+    std::cout << "Hello blabla \n";
+    return;
+}
