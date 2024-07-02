d233be71168794ffc8383e84983539606b4e8024
vietbui1502 <buivanviet93@gmail.com>
2024-07-02 23:58:03 +0700

    External library: Git submodule

--- app/main.cc
+++ app/main.cc
@@ -1,10 +1,18 @@
 #include <iostream>
 
+//#include <nlohmann/json.hpp>
+
 #include "my_lib.h"
 #include "config.hpp"
 
 int main() {
     std::cout << project_name << '\n';
+    std::cout << project_version << '\n';
+
+    std::cout << "JSON lib version:"
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << "\n";
     print_blabla();
     return 0;
 }
